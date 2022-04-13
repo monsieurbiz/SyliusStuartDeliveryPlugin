6ceefa71426678138700e1a73e2c17449e058972
David Buros <d.buros@monsieurbiz.com>
2022-04-13 15:47:38 +0200

    feat: add calculator and calculator configuration for Stuart delivery

---
+++ src/Exception/StuartDeliveryException.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Stuart delivery plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusStuartDeliveryPlugin\Exception;
+
+use RuntimeException;
+
+final class StuartDeliveryException extends RuntimeException
+{
+}
