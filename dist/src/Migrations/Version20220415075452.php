16fc7bca8d3474f937e5034a6a94e71c468cc493
Maxime Huran <m.huran@monsieurbiz.com>
2022-06-08 17:01:06 +0200

    feat: create messenger table only if not exists

--- dist/src/Migrations/Version20220415075452.php
+++ dist/src/Migrations/Version20220415075452.php
@@ -29,7 +29,9 @@ final class Version20220415075452 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        if (!$schema->hasTable('messenger_messages')) {
+            $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        }
         $this->addSql('ALTER TABLE sylius_order ADD stuart_job_id INT DEFAULT NULL');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_6196A1F97A67FD1 ON sylius_order (stuart_job_id)');
     }
